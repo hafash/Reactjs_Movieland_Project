c2340bc468631336103483b610537b4deebea2e8
HAFASHIMANA Germain <89031303+hafash@users.noreply.github.com>
2024-04-02 07:43:58 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+import App from './App';
+import SearchIcon from './search.svg';
+
+ReactDOM.render(<App />, document.getElementById('root'));
